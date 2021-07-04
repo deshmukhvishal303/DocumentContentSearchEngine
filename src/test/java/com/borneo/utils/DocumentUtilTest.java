5cd318183b239e754d77050a956cdb25ae02b355
vishaldeshmukh <deshmukhvishal303@gmail.com>
2021-07-05 03:38:40 +0530

    Removing unused import

--- src/test/java/com/borneo/utils/DocumentUtilTest.java
+++ src/test/java/com/borneo/utils/DocumentUtilTest.java
@@ -10,7 +10,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
